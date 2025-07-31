9a9eb3bc2b2703b25cd9a6b94d831dd288b590fe
lydat1502 <156647074+lydat1502@users.noreply.github.com>
2025-07-31 07:11:53 +0700

    add api

---
+++ CSW306_ProjectAPI/CSW306_ProjectAPI/Models/CSW306_ProjectAPIContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace CSW306_ProjectAPI.Models
+{
+    public class CSW306_ProjectAPIContext : DbContext
+    {
+        public CSW306_ProjectAPIContext(DbContextOptions<CSW306_ProjectAPIContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+    }
+}
